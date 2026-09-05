bfc91ca13e7ef1980a8818ff687f61e2b6b3117f
cb341 <daniel@bengl.cz>
2026-09-05 20:12:28 +0200

    w

--- single_file.lean
+++ single_file.lean
@@ -58,7 +58,11 @@ namespace MyNat
 instance : Inhabited MyNat where
   default := MyNat.zero
 
-def ofNat (x : Nat) : MyNat :=
+-- We define ℕ by induction.
+--   Base case. '0' (zero).
+--   Inductive step. Successor of x ∈ ℕ
+-- From now on the universe of discourse is ℕ.
+def ofNat (x : Nat)   : MyNat :=
   match x with
   | Nat.zero   => MyNat.zero
   | Nat.succ b => MyNat.succ (ofNat b)
@@ -76,32 +80,22 @@ instance : ToString MyNat where
 
 theorem zero_eq_0 : MyNat.zero = 0 := rfl
 
+-- we define '1' to be the successor of '0'
 def one : MyNat := MyNat.succ 0
-
-def pred : ℕ → ℕ
-| 0 => 37 -- random (garbage) value according to NNG4
-| succ n => n
-
-lemma pred_succ (n : ℕ) : pred (succ n) = n := rfl
 
 def is_zero : ℕ → Prop
 | 0 => True
 | succ _ => False
 
-lemma is_zero_zero : is_zero 0 = True := rfl
-lemma is_zero_succ (n : ℕ) : is_zero (succ n) = False := rfl
-
-theorem zero_ne_succ (a : ℕ) : 0 ≠ succ a := by
-  intro h
-  rewrite[← is_zero_succ a]
-  rewrite[← h]
-  rewrite[is_zero_zero]
-  trivial
-
-theorem one_eq_succ_zero : 1 = succ 0 := by rfl
-theorem two_eq_succ_one : 2 = succ 1 := by rfl
-theorem three_eq_succ_two : 3 = succ 2 := by rfl
+-- Number definitions.
+-- we define '7' to be the successor of '6' which is the successor of '5' which is the succ of '6' ...
+theorem one_eq_succ_zero   : 1 = succ 0 := by rfl
+theorem two_eq_succ_one    : 2 = succ 1 := by rfl
+theorem three_eq_succ_two  : 3 = succ 2 := by rfl
 theorem four_eq_succ_three : 4 = succ 3 := by rfl
+theorem five_eq_succ_four  : 5 = succ 4 := by rfl
+theorem six_eq_succ_five   : 6 = succ 5 := by rfl
+theorem seven_eq_succ_six  : 7 = succ 6 := by rfl
 
 -- addition
 opaque add : MyNat → MyNat → MyNat
@@ -109,10 +103,15 @@ opaque add : MyNat → MyNat → MyNat
 instance instAdd : Add MyNat where
   add := MyNat.add
 
+-- Inductive addition:
+-- Axiom 1 (add_zero). a + 0 = a
+-- Axiom 2 (add_succ). a + (succ d) = succ (a + d)
 axiom add_zero (a : MyNat) : a + 0 = a
 axiom add_succ (a d : MyNat) : a + (succ d) = succ (a + d)
 
 -- inequality
+-- Inequality:
+-- Axiom 3 (le). if there exists c, such that a + c = b, then a ≤ b.
 def le (a b : ℕ) :=  ∃ (c : ℕ), b = a + c
 instance : LE MyNat := ⟨MyNat.le⟩
 -- END NNG4 --
@@ -127,12 +126,24 @@ instance : LE MyNat := ⟨MyNat.le⟩
 
 -- BEGIN DANI --
 
+-- Lemma. (succ_eq_ad_one). succ n = n + 1.
+-- Proof. In 'Number definitions' we defined '1' to be the successor of '0'.
+-- By the axiom 2 of addition, n + succ ( 0 ) iff succ( n + 0 ).
+-- By unfolding the axiom 1 of addition we get succ ( n ), showing that succ n = n + 1. QED
 theorem succ_eq_add_one n : succ n = n + 1 := by
   rewrite[one_eq_succ_zero]
   rewrite[add_succ]
   rewrite[add_zero]
   rfl
 
+-- Lemma. (zero_add). 0 + n = n.
+-- Proof. By mathematical induction.
+--   Base case. n = zero. By the axiom 1 (add_zero) of addition, we show that 0 + n = n.
+--   Induction step. Let d be arbitrary.
+--     n = succ ( d ). By axiom 2 (add_succ) of addition, we show that  0 + succ (d) = succ ( 0 + d).
+--     By axiom 1 (add_zero), we conclude that 0 + succ d = succ d.
+--  As the induction is exhaustive, we have proven that 0 + n = n. QED
+--
 theorem zero_add (n : ℕ) : 0 + n = n := by
   induction n with
   | zero =>
@@ -144,6 +155,11 @@ theorem zero_add (n : ℕ) : 0 + n = n := by
     rewrite[hd]
     rfl
 
+-- Lemma. (succ_add). succ a + b = succ (a + b).
+-- Proof. By mathematical induction on b, fixing a.
+--   Base case: b = zero. Goal: succ a + b
+--   Inductive step: Let d be arbitrary. b = succ d.
+--   Then
 theorem succ_add (a b : ℕ) : succ a + b = succ (a + b) := by
   induction b with
   | zero =>
@@ -194,6 +210,48 @@ theorem le_succ_self (x : ℕ) : x ≤ succ x := by
   use 1
   exact succ_eq_add_one x
 
+-- Theorem. (le_total) for all natural numbers x and y, either x is greater or equal to y or y is greater than or equal to x.
+--
+--
+-- Definitions.
+--
+-- NUMBERS
+--   We define ℕ by induction.
+--     Base case. '0' (zero).
+--     Inductive step. Successor of x ∈ ℕ
+--   From now on the universe of discourse is ℕ.
+--
+-- Number definitions.
+-- we define '7' to be the successor of '6' which is the successor of '5' which is the succ of '6' ...
+-- Specifically number strings in the order ('0',1','2','3','4','5','6','7')
+--
+--   Peano arithmetic:
+--   Axiom 1 (add_zero). a + 0 = a
+--   Axiom 2 (add_succ). a + (succ d) = succ (a + d)
+--
+--   Inequality.
+-- Axiom 3 (le). if there exists c, such that a + c = b, then a ≤ b.
+--
+-- LEMMAS.
+--
+-- Lemma. (succ_eq_ad_one). succ n = n + 1.
+-- Proof. In 'Number definitions' we defined '1' to be the successor of '0'.
+-- By the axiom 2 of addition, n + succ ( 0 ) iff succ( n + 0 ).
+-- By unfolding the axiom 1 of addition we get succ ( n ), showing that succ n = n + 1. QED
+--
+-- Lemma. (zero_add). 0 + n = n.
+-- Proof. By mathematical induction.
+--   Base case. n = zero. By the axiom 1 (add_zero) of addition, we show that 0 + n = n.
+--   Induction step. Let d be arbitrary.
+--     n = succ ( d ). By axiom 2 (add_succ) of addition, we show that  0 + succ (d) = succ ( 0 + d).
+--     By axiom 1 (add_zero), we conclude that 0 + succ d = succ d.
+--  As the induction is exhaustive, we have proven that 0 + n = n. QED
+--
+-- Proof. we use mathematical induction.
+--   Base case: []
+--   Induction step: []
+--
+
 -- This is the climax - the proof we have been approaching thus far
 theorem le_total (x y : ℕ) : x ≤ y ∨ y ≤ x := by
   induction y with
@@ -226,5 +284,7 @@ theorem le_total (x y : ℕ) : x ≤ y ∨ y ≤ x := by
         use a
         rewrite[succ_add]
         rfl
+
+#check le_total 6 7
 
 end MyNat
