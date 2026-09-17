7e307e0dc6ee59cf1bf0104cd444851eb58212ed
cb341 <daniel@bengl.cz>
2026-09-17 02:03:24 +0200

    Link Markdown sources from rendered pages

--- _plugins/raw_markdown.rb
+++ _plugins/raw_markdown.rb
@@ -1,15 +1,25 @@
 require "fileutils"
 
+module RawMarkdown
+  def self.pages(site)
+    pages = site.pages + site.collections.values.flat_map(&:docs).select(&:write?)
+    pages.select do |page|
+      page.url != "/" && File.extname(page.path.to_s).casecmp(".md").zero?
+    end
+  end
+end
+
+Jekyll::Hooks.register :site, :post_read do |site|
+  RawMarkdown.pages(site).each do |page|
+    page.data["raw_url"] = "#{page.url.chomp("/")}.md"
+  end
+end
+
 Jekyll::Hooks.register :site, :post_write do |site|
-  pages = site.pages + site.collections.values.flat_map(&:docs).select(&:write?)
-
-  pages.each do |page|
-    next unless File.extname(page.path.to_s).casecmp(".md").zero?
-
-    html_path = page.destination(site.dest)
-    next unless html_path.end_with?(".html")
-
+  RawMarkdown.pages(site).each do |page|
     source_path = File.expand_path(page.path, site.source)
-    FileUtils.cp(source_path, html_path.sub(/\.html\z/, ".md"))
+    raw_path = File.join(site.dest, page.data.fetch("raw_url").delete_prefix("/"))
+    FileUtils.mkdir_p(File.dirname(raw_path))
+    FileUtils.cp(source_path, raw_path)
   end
 end
