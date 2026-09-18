e3c973c174c3e2253b93cad663905bbb0d51076a
cb341 <daniel@bengl.cz>
2026-09-18 18:49:46 +0200

    Selectability

--- bin/render-math-svg.mjs
+++ bin/render-math-svg.mjs
@@ -42,14 +42,29 @@ const document = mathjax.document(readFileSync(0, "utf8"), {
 
 await document.renderPromise();
 
-const errors = adaptor.tags(adaptor.root(document.document), "g")
+const root = adaptor.root(document.document);
+const errors = adaptor.tags(root, "g")
   .filter((node) => adaptor.getAttribute(node, "data-mml-node") === "merror");
 if (errors.length > 0) {
   const messages = errors.map((node) => adaptor.getAttribute(node, "data-mjx-error"));
   throw new Error(`MathJax could not render:\n${messages.join("\n")}`);
 }
 
-for (const background of adaptor.tags(adaptor.root(document.document), "rect")) {
+for (const container of adaptor.tags(root, "mjx-container")) {
+  const math = adaptor.tags(container, "g")
+    .find((node) => adaptor.getAttribute(node, "data-mml-node") === "math");
+  const source = math && adaptor.getAttribute(math, "data-latex");
+  if (!source) continue;
+
+  const selectableSource = source.replace(/\r?\n[\t ]*/g, " ");
+  const text = adaptor.node("span", {
+    "aria-hidden": "true",
+    class: "math-tex-source"
+  }, [adaptor.text(selectableSource)]);
+  adaptor.append(container, text);
+}
+
+for (const background of adaptor.tags(root, "rect")) {
   if (adaptor.getAttribute(background, "data-bgcolor") !== "true") continue;
 
   const content = adaptor.next(background);
@@ -59,7 +74,7 @@ for (const background of adaptor.tags(adaptor.root(document.document), "rect")) 
   }
 }
 
-const html = adaptor.outerHTML(adaptor.root(document.document))
+const html = adaptor.outerHTML(root)
   .replace(/@font-face \/\* zero \*\/ \{[\s\S]*?\n\}/, "");
 
 process.stdout.write(adaptor.doctype(document.document));
